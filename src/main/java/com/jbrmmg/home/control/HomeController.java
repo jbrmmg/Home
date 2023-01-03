66ba9c24dda0fe6a1a620fba4b8ee80c6d1bb5d7
jason <jason@jbrmmg.me.uk>
2023-01-03 14:43:51 +0000

    Changes

--- src/main/java/com/jbrmmg/home/control/HomeController.java
+++ src/main/java/com/jbrmmg/home/control/HomeController.java
@@ -1,11 +1,21 @@
 package com.jbrmmg.home.control;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jbrmmg.home.dijkstras.StopCalculator;
+import com.jbrmmg.home.octopus.OctopusManager;
+import com.jbrmmg.home.octopus.Response;
 import com.jbrmmg.home.tfl.TflManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/jbr/int/home")
@@ -13,11 +23,14 @@ public class HomeController {
     private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
 
     public final TflManager tflManager;
+
+    public final OctopusManager octopusManager;
     public final StopCalculator stopCalculator;
 
     @Autowired
-    public HomeController(TflManager tflManager, StopCalculator stopCalculator) {
+    public HomeController(TflManager tflManager, OctopusManager octopusManager, StopCalculator stopCalculator) {
         this.tflManager = tflManager;
+        this.octopusManager = octopusManager;
         this.stopCalculator = stopCalculator;
     }
 
@@ -38,5 +51,19 @@ public class HomeController {
         LOG.info("Get routes");
 
         return stopCalculator.find(station,stops,zones);
+    }
+
+    @GetMapping(path="/gas")
+    public @ResponseBody Response gas(@RequestParam(value="Key") String key,
+                                      @RequestParam(value="mprn") String mprn,
+                                      @RequestParam(value="serial") String serial) throws IOException {
+        return octopusManager.gas(key, mprn, serial);
+    }
+
+    @GetMapping(path="/electricity")
+    public @ResponseBody Response electricity(@RequestParam(value="Key") String key,
+                                      @RequestParam(value="mpan") String mpan,
+                                      @RequestParam(value="serial") String serial) throws IOException {
+        return octopusManager.electricity(key, mpan, serial);
     }
 }
---
+++ src/main/java/com/jbrmmg/home/octopus/Response.java
@@ -0,0 +1,10 @@
+package com.jbrmmg.home.octopus;
+
+import java.util.List;
+
+public class Response {
+    public int count;
+    public String next;
+    public String previous;
+    public Reading[] results;
+}

c09a4eb7e145cb5a136557395c04f7b474fa57a0
jbrmmg <jason@jbrmmg.me.uk>
2026-06-28 17:20:59 +0100

    JBR-662 - improvements to API and routes.

---
+++ src/main/java/com/jbrmmg/home/data/MergedRouteRepository.java
@@ -0,0 +1,7 @@
+package com.jbrmmg.home.data;
+
+import com.jbrmmg.home.data.entity.MergedRoute;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MergedRouteRepository extends JpaRepository<MergedRoute, String> {
+}
